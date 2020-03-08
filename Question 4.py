71a333dd01cac86a43b8f87b9692c9c85df486b5
Mattia Calzetta <51382121+mcalzett@users.noreply.github.com>
2020-03-08 12:30:38 +0000

    Create Question 4.py

---
+++ Question 4.py
@@ -0,0 +1,73 @@
+#FIRST QUESTION
+
+portfolio_value = 16000000
+mean_returns = 1/100
+vol_returns = 3/100
+mean_spread = round(0.35/100,4)
+vol_spread = 1.5/100
+factor = 2.33
+
+VAR = portfolio_value*(-mean_returns + factor * vol_returns)
+LVAR = portfolio_value*(-mean_returns + factor * vol_returns + 0.5*(mean_spread + factor*vol_spread))
+LVAR_MINUS_VAR = LVAR - VAR
+
+print('====== PARAMETRIC VAR - Question A ======')
+print('portfolio_value: ' + str(portfolio_value))
+print('mean_returns: ' + str(mean_returns))
+print('vol_returns: ' + str(vol_returns))
+print('mean_spread: ' + str(mean_spread))
+print('vol_spread: ' + str(vol_spread))
+print('factor: ' + str(factor))
+print('VAR: ' + str(VAR))
+print('LVAR: ' + str(LVAR))
+print('LVAR_MINUS_VAR: ' + str(LVAR_MINUS_VAR))
+
+
+#NOW SECOND QUESTION - UK GILTS
+
+portfolio_value = 40000000
+mean_returns = 0/100
+vol_returns = 3/100
+mean_spread = round(0.15/100,4)
+vol_spread = 0/100
+factor = 2.33
+
+VAR = portfolio_value*(-mean_returns + factor * vol_returns)
+LVAR = portfolio_value*(-mean_returns + factor * vol_returns + 0.5*(mean_spread + factor*vol_spread))
+LVAR_MINUS_VAR = LVAR - VAR
+
+print('====== PARAMETRIC VAR - Question B.1 ======')
+print('portfolio_value: ' + str(portfolio_value))
+print('mean_returns: ' + str(mean_returns))
+print('vol_returns: ' + str(vol_returns))
+print('mean_spread: ' + str(mean_spread))
+print('vol_spread: ' + str(vol_spread))
+print('factor: ' + str(factor))
+print('VAR: ' + str(VAR))
+print('LVAR: ' + str(LVAR))
+print('LVAR_MINUS_VAR: ' + str(LVAR_MINUS_VAR))
+
+
+portfolio_value = 40000000
+mean_returns = 0/100
+vol_returns = 3/100
+mean_spread = round(1.25/100,4)
+vol_spread = 0/100
+factor = 2.33
+
+VAR = portfolio_value*(-mean_returns + factor * vol_returns)
+LVAR = portfolio_value*(-mean_returns + factor * vol_returns + 0.5*(mean_spread + factor*vol_spread))
+LVAR_MINUS_VAR = LVAR - VAR
+
+print('====== PARAMETRIC VAR - Question B.2 ======')
+print('portfolio_value: ' + str(portfolio_value))
+print('mean_returns: ' + str(mean_returns))
+print('vol_returns: ' + str(vol_returns))
+print('mean_spread: ' + str(mean_spread))
+print('vol_spread: ' + str(vol_spread))
+print('factor: ' + str(factor))
+print('VAR: ' + str(VAR))
+print('LVAR: ' + str(LVAR))
+print('LVAR_MINUS_VAR: ' + str(LVAR_MINUS_VAR))
+
+
